621a90c5f77876c689f4dea22d86e1c842ed5264
Trevor Hanson <52029257+trucetht@users.noreply.github.com>
2020-07-01 14:51:16 -0400

    Add files via upload

---
+++ Strings_Comments_StringInterpolation/Strings_Comments_StringInterpolation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Strings_Comments_StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Trevor"; // a string variable that represents my first name
+            string lastName = "Hanson"; // a string variable that represents my last name
+
+            Console.WriteLine($"Hello, my name is {firstName} {lastName}");
+
+            
+        }
+    }
+}
